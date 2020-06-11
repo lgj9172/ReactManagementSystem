d1438c7bbda5bf52dff3d25a2146cf94113a09d9
lgj9172 <lgj9172@gmail.com>
2020-06-11 14:35:07 +0900

    Making customer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = {
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
 
 class App extends React.Component {
   render(){
     return(
-      <div className="gray-background">
-        <img src={logo} alt="logo"/>
-        <h2>Let's develop management system!</h2>
-      </div>
-    )
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
   }
 }
 
